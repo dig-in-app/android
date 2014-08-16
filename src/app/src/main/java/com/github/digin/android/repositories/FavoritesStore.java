7a54740a3319969f747e2854225306181141dcd2
Michael Hockerman <mike@hockerman.com>
2014-08-16 16:17:59 -0400

    Fixed bug with favorites not storing properly.

--- src/app/src/main/java/com/github/digin/android/repositories/FavoritesStore.java
+++ src/app/src/main/java/com/github/digin/android/repositories/FavoritesStore.java
@@ -6,6 +6,7 @@ import android.preference.Preference;
 import android.provider.Telephony;
 
 import com.github.digin.android.listeners.OnParticipantQueryListener;
+import com.github.digin.android.logging.Logger;
 import com.github.digin.android.models.Brewery;
 import com.github.digin.android.models.Chef;
 import com.github.digin.android.models.Participant;
@@ -31,19 +32,22 @@ public abstract class FavoritesStore {
 
         Set<String> favorites = prefs.getStringSet(chefSetPrefId, null);
         if (favorites == null) {
+            Logger.log(FavoritesStore.class, "Favorites do not exist, creating new favorites.");
             favorites = new HashSet<String>();
         }
 
         // Make sure the chef isn't already a favorite
         if (contains(context, chef)) {
+            Logger.log(FavoritesStore.class, "Favorites already contains that favorite.");
             return;
         }
 
         // Store the ID
-        favorites.add(chef.getId());
+        Set<String> copy = new HashSet<String>(favorites);
+        copy.add(chef.getId());
 
         // Recommit to the shared prefs
-        prefs.edit().putStringSet(chefSetPrefId, favorites).commit();
+        prefs.edit().putStringSet(chefSetPrefId, copy).commit();
 
     }
 
@@ -63,21 +67,29 @@ public abstract class FavoritesStore {
     }
 
     public static void removeFavorite(Context context, Participant chef) {
+        Logger.log(FavoritesStore.class, "Removing " + chef.getName() + " from store");
 
         SharedPreferences prefs = context.getSharedPreferences(prefsName, 0);
 
         Set<String> favorites = prefs.getStringSet(chefSetPrefId, null);
         if (favorites == null) {
+            Logger.log(FavoritesStore.class, "Favorites storage does not exist. Cannot remove");
             return;
         }
 
         if (!contains(context, chef)) {
+            Logger.log(FavoritesStore.class, "Requested unfavorite was never favorited.");
             return;
         }
 
-        favorites.remove(chef.getId());
+        Set<String> copy = new HashSet<String>(favorites);
+        copy.remove(chef.getId());
 
-        prefs.edit().putStringSet(chefSetPrefId, favorites).commit();
+        if (copy.size() == 0) {
+            prefs.edit().remove(chefSetPrefId).commit();
+        } else {
+            prefs.edit().putStringSet(chefSetPrefId, copy).commit();
+        }
 
     }
 
@@ -85,8 +97,18 @@ public abstract class FavoritesStore {
 
         SharedPreferences prefs = context.getSharedPreferences(prefsName, 0);
 
-        final Set<String> favorites = prefs.getStringSet(chefSetPrefId, null);
-        if (favorites == null) {
+        Set<String> original = prefs.getStringSet(chefSetPrefId, null);
+
+        if (original == null) {
+            if (listener != null) {
+                listener.onComplete(new LinkedList<Participant>());
+            }
+            return;
+        }
+
+        final Set<String> favorites = new HashSet<String>(original);
+
+        if (favorites.size() == 0) {
             if (listener != null) {
                 listener.onComplete(new LinkedList<Participant>());
             }
@@ -94,7 +116,6 @@ public abstract class FavoritesStore {
         }
 
         final List<Participant> participants = new LinkedList<Participant>();
-
 
         ChefsStore.batchGetChefById(context, favorites, new OnParticipantQueryListener<Chef>() {
             @Override
